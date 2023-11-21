1d677a911d1f3571dc33e2a505a53b156e58f5d1
LorandSarkozi <lorand.sarkozi25@yahoo.com>
2023-11-21 08:09:07 +0200

    lab7_tema_new

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,27 +1,17 @@
 import controller.LoginController;
-import database.DatabaseConnectionFactory;
 import database.JDBConnectionWrapper;
 import javafx.application.Application;
 import javafx.stage.Stage;
-import model.Book;
-import model.builder.BookBuilder;
 import model.validator.UserValidator;
-import repository.book.BookRepository;
-import repository.book.BookRepositoryCacheDecorator;
-import repository.book.BookRepositoryMySQL;
-import repository.book.Cache;
 import repository.security.RightsRolesRepository;
 import repository.security.RightsRolesRepositoryMySQL;
 import repository.user.UserRepository;
 import repository.user.UserRepositoryMySQL;
-import service.book.BookService;
-import service.book.BookServiceImpl;
 import service.user.AuthenticationService;
-import service.user.AuthenticationServiceMySQL;
+import service.user.AuthenticationServiceImpl;
 import view.LoginView;
 
 import java.sql.Connection;
-import java.time.LocalDate;
 
 import static database.Constants.Schemas.PRODUCTION;
 
@@ -37,7 +27,7 @@ public class Main extends Application {
         final RightsRolesRepository rightsRolesRepository = new RightsRolesRepositoryMySQL(connection);
         final UserRepository userRepository = new UserRepositoryMySQL(connection, rightsRolesRepository);
 
-        final AuthenticationService authenticationService = new AuthenticationServiceMySQL(userRepository,
+        final AuthenticationService authenticationService = new AuthenticationServiceImpl(userRepository,
                 rightsRolesRepository);
 
         final LoginView loginView = new LoginView(primaryStage);
